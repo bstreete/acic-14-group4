cae773b1537d15cd9610736f2041d30d0537a6b5
Brayden Streeter <bstreete@email.arizona.edu>
2014-12-10 18:18:03 -0700

    Changed path on gdal_merge.py

--- src/eemt_queue.py
+++ src/eemt_queue.py
@@ -260,7 +260,7 @@ def merge_years(wq, input_dir, output_dir, start, end):
 		t = Task(' '.join(command))
 
 		# Specify the executable and output files
-		t.specify_input_file('gdal_merge.py', 'gdal_merge.py')
+		t.specify_input_file('~/gdal/bin/gdal_merge.py', 'gdal_merge.py')
 		t.specify_output_file(output_dir + 'trad_%d.tif' % year, 'trad_%d.tif' % year)
 
 		for day in range(1, 366): 
